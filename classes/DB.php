aa691a1b3c1ec206783aaead37c20f278ebfe802
i-Tutu <ignatiustutu@gmail.com>
2021-01-25 17:16:40 +0000

    Added: DB

---
+++ classes/DB.php
@@ -0,0 +1,30 @@
+<?php 
+
+	/**
+	 * 
+	 */
+	class DB{
+
+		private static function connect(){
+
+			$pdo = new pdo('mysql:host = 127.0.0.1;dbname=socialmedia;charset = utf8', 'root', '');
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			return $pdo;
+
+		}
+
+		public static function query($query, $params = array()){
+
+			$statement = self::connect()->prepare($query);
+			$statement->execute($params);
+
+			if (explode(' ', $query)[0] == 'SELECT'){
+
+				$data = $statement->fetchAll();
+				return $data;
+
+			}
+		}
+	}
+
+?>
